fbab30a55cd095932a81b923ad891b679a0f0f18
mishatim <mishatim@yahoo.com>
2018-02-23 18:12:07 -0500

    Source for #16

---
+++ LineGetter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearcherForHuddle
+{
+    class LineGetter
+    {
+        public string GetLine(String file, int N)
+        {
+            string line = File.ReadLines(file).Skip(N).Take(1).First();
+            return line;
+        }
+    }
+}
